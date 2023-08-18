31136583ae4145bf5742cd61fc82565d6493466d
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-18 12:32:12 +0500

    pvc roofing

--- header.php
+++ header.php
@@ -99,6 +99,7 @@
                                         <a href="<?php echo $base_url; ?>/sbs-roofing.php">SBS Roofing System</a>
                                         <a href="<?php echo $base_url; ?>/tpo-roofing.php">TPO Roofing System</a>
                                         <a href="<?php echo $base_url; ?>/epdm-roofing.php">EPDM Roofing</a>
+                                        <a href="<?php echo $base_url; ?>/pvc-roofing.php">PVC Roofing </a>
                                         <a href="<?php echo $base_url; ?>/roof-deck.php">Lightweight Insulating Concrete
                                             (LWIC) Roof Decks</a>
                                         <!-- <a href="#">Reducing Lateral Forces</a> -->
---
+++ pvc-roofing.php
@@ -0,0 +1,78 @@
+<title>PVC Commercial Roofing</title>
+<?php include('header.php'); ?>
+<div class="clearfix border-black"></div>
+<div class="container margin-bottom-lg margin-top-lg">
+    <div class="img-wrap"><img src="<?php echo $base_url; ?>/images/PVC Roofing Command.JPG" style="width:65%"
+            alt="pvc">
+    </div>
+    <br>
+
+    <h1>PVC Commercial Roofing System</h1>
+    <p>PVC (Polyvinyl Chloride) commercial roofing is another popular roofing material with several advantages for
+        commercial and industrial applications. Here are the key advantages of using PVC roofing:</p>
+
+    <ul>
+        <li><strong>Durability:</strong> PVC roofing is highly durable and resistant to various environmental factors,
+            such as UV radiation, chemicals, and fire. It can withstand extreme temperatures and weather conditions,
+            making it suitable for both hot and cold climates.</li>
+        <li><strong>Waterproofing:</strong> PVC roofing is inherently waterproof, providing an effective barrier against
+            water infiltration. It is heat-welded at the seams, creating a watertight seal that prevents leaks and
+            ensures the roof's long-term integrity.</li>
+        <li><strong>Chemical Resistance:</strong> PVC is highly resistant to chemicals, including acids, alkalis, and
+            pollutants. This makes it an excellent choice for commercial and industrial buildings where chemical
+            exposure is a concern.</li>
+        <li><strong>Low Maintenance:</strong> PVC roofing requires minimal maintenance over its lifespan. Regular
+            inspections and occasional cleaning are typically sufficient to keep the roof in good condition.</li>
+        <li><strong>Energy Efficiency:</strong> PVC roofing is available in white or light-colored membranes, which have
+            reflective properties. This reflectivity helps to reduce heat absorption, leading to lower cooling costs and
+            improved energy efficiency for the building.</li>
+        <li><strong>Flexibility:</strong> PVC roofing is flexible and can adapt well to various roof shapes and designs.
+            It can accommodate complex roof structures without compromising its performance.</li>
+        <li><strong>Long Lifespan:</strong> When properly installed and maintained, PVC roofing can have a long service
+            life, often lasting 20 years or more.</li>
+        <li><strong>Resistance to Fungi and Bacteria:</strong> PVC roofing is resistant to fungi, bacteria, and algae
+            growth, which can be beneficial in humid or moist climates.</li>
+        <li><strong>Lightweight:</strong> PVC roofing is relatively lightweight compared to some other roofing
+            materials, reducing the overall structural load on the building.</li>
+        <li><strong>Recyclable:</strong> PVC is a recyclable material, making PVC roofing a more sustainable choice
+            compared to some other roofing options.</li>
+        <li><strong>Ease of Repairs:</strong> PVC roofing can be installed quickly and efficiently, especially when
+            using heat-welding techniques. In the event of damage, repairs can also be made easily by heat-welding
+            patches or sealants.</li>
+        <li><strong>Code Compliance:</strong> PVC roofing often meets various building codes and standards, ensuring
+            compliance with local regulations.</li>
+        <li><strong>For New Roofs:</strong>PVC roofing is commonly used for new construction projects in commercial and
+            industrial buildings. Its
+            durability, flexibility, and ease of installation make it an attractive option for architects and
+            contractors
+            working on new building designs. PVC roofing can be installed over different types of roof decks and can
+            accommodate complex roof structures, providing a seamless and watertight solution for the building.</li>
+        <li><strong>For Reroofing:</strong>PVC roofing is also an excellent choice for reroofing or roof replacement
+            projects. When an existing roof needs
+            to be replaced, PVC membranes can be installed directly over the old roofing system, saving time and labor
+            costs. This method is often known as a "roof-over" or "recover" installation. <br>
+            Additionally, the heat-welding installation technique used for PVC roofing allows for easy and efficient
+            seam
+            bonding, ensuring a watertight roof without the need for adhesives or mechanical fasteners. This can be
+            especially advantageous when reroofing existing buildings, as it minimizes disruptions to building occupants
+            during the installation process.
+        </li>
+    </ul>
+
+
+    <p>Overall, PVC commercial roofing provides a combination of durability, waterproofing, energy efficiency, and ease
+        of installation that makes it an attractive choice for commercial and industrial buildings.</p>
+
+    <p><strong> <em>Command Roofing</em> is your trusted roofing partner, offering dependable and durable commercial and
+            industrial roofing solutions that go beyond your expectations. Our mission is to provide top-notch services
+            that
+            you can rely on for all your roofing needs.</strong></p>
+
+    <br>
+    <p><a href="<?php echo $base_url; ?>/contact.php" class="btn btn-default">Contact Us </a> </p>
+    <br>
+
+</div>
+<div class="clearfix"></div>
+
+<?php include('footer.php'); ?>

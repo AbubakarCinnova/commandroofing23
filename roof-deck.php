bd67ea9e640e76f0617e2497d5561222f04624e1
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-22 15:47:03 +0500

    updated roof deck

---
+++ roof-deck.php
@@ -0,0 +1,72 @@
+<title>Lightweight Insulating Concrete Roof Deck System | Command Roofing</title>
+<meta name="description"
+    content=" A Lightweight Insulating Concrete Roof Deck System is an advanced roofing solution designed specifically for commercial and industrial buildings." />
+<?php include('header.php'); ?>
+<div class="clearfix border-black"></div>
+<div class="container margin-bottom-lg margin-top-lg">
+    <div class="img-wrap"><img src="<?php echo $base_url; ?>/images/concrete roof decks dayton ohio.jpg"
+            style="width:65%" alt="roof decks">
+    </div>
+    <br>
+    <h1>Lightweight Cellular Insulating Concrete Roof Deck System</h1>
+
+    <p>A Lightweight Insulating Concrete Roof Deck System is an advanced roofing solution designed specifically for
+        commercial and industrial buildings. This system involves the installation of a layer of lightweight insulating
+        concrete that contains a high proportion of entrained air, resulting in a cellular structure. This cellular
+        nature significantly reduces the overall weight of the concrete while enhancing its insulating properties.</p>
+
+    <h2>Benefits of a Lightweight Insulating Concrete Roof Deck System for Commercial Buildings:</h2>
+
+    <ul>
+        <li><strong>Reduced Weight:</strong> One of the primary benefits of this system is its lightweight nature. The
+            cellular structure of the insulating concrete reduces the density of the material, resulting in a
+            significantly lighter roof deck compared to traditional concrete systems. This reduced weight helps minimize
+            the load on the building's structure, making it suitable for both new constructions and retrofit
+            applications.</li>
+        <li><strong>Excellent Insulation:</strong> The entrained air in the lightweight cellular insulating concrete
+            acts as an effective insulator. This helps regulate indoor temperatures by reducing heat transfer through
+            the roof. The enhanced insulation properties can lead to energy savings by reducing heating and cooling
+            demands.</li>
+        <li><strong>Ponding Prevention:</strong> Lightweight cellular insulating concrete is designed to have a higher
+            slope-to-drain ratio compared to conventional concrete. This feature helps prevent ponding, which occurs
+            when water accumulates on the roof due to inadequate drainage. Ponding can lead to water infiltration,
+            structural damage, and even the growth of algae or mold. The improved drainage capability of the cellular
+            system helps mitigate these risks.</li>
+        <li><strong>Slope Correction:</strong> Similar to the composite lightweight insulating concrete system, the
+            lightweight cellular variant can also be used to correct minor roof slope irregularities. The material's
+            fluid nature during installation allows for easy leveling, ensuring proper water drainage and preventing
+            ponding.</li>
+        <li><strong>Sound Insulation:</strong> The cellular structure of the lightweight insulating concrete contributes
+            to sound insulation, helping to reduce external noise penetration into the building. This can create a
+            quieter and more comfortable indoor environment.</li>
+        <li><strong>Fire Resistance:</strong> Lightweight cellular insulating concrete often includes fire-resistant
+            additives, enhancing the overall fire resistance of the roof assembly. This is a crucial safety feature for
+            commercial buildings.</li>
+        <li><strong>Longevity:</strong> When properly installed and maintained, a Lightweight Cellular Insulating
+            Concrete Roof Deck System can offer a long service life. The cellular structure can help minimize cracking
+            and improve the system's durability over time.</li>
+        <li><strong>Environmental Benefits:</strong> The lightweight nature of the system reduces the environmental
+            impact associated with transportation and installation, as well as the overall load on the building.
+            Additionally, enhanced energy efficiency contributes to sustainable building practices.</li>
+    </ul>
+    <br>
+
+    <p><em><strong>At Command Roofing, we stand as your reliable roofing ally, delivering resilient and lasting
+                solutions for
+                commercial and industrial rooftops that exceed every anticipation. Our commitment is to furnish
+                unparalleled
+                services that instill unwavering trust, addressing all your demands for commercial roofing with the
+                utmost
+                precision and care. With expertise in lightweight cellular insulating concrete, we're dedicated to
+                elevating
+                your roofing experience to new heights.</strong></em></p>
+
+
+    <br>
+    <p><a href="<?php echo $base_url; ?>/contact.php" class="btn btn-default">Contact Us </a> </p>
+    <br>
+
+</div>
+<div class="clearfix"></div>
+
+<?php include('footer.php'); ?>

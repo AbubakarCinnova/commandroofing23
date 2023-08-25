a0a1a9d77f34804899640fb0fe6ced28edc80175
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-25 12:46:27 +0500

    video banner

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php include('header.php'); ?>
 
 <div class="video-banner">
-    <iframe src="<?php echo $base_url; ?>/video/1070194624-hd.mov" frameborder="0"></iframe>
+    <video width="640" height="360" controls>
+        <source src="<?php echo $base_url; ?>/video/command-banner-video.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
 </div>
 <!-- Carousel
     ================================================== -->

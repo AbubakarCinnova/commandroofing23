33f17d9291235d9bccbc319a8c62124d95dc6af8
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-25 12:33:09 +0500

    updated video

--- index.php
+++ index.php
@@ -1,28 +1,18 @@
 <?php include('header.php'); ?>
+
+<div class="video-banner">
+    <video width="640" height="360" controls>
+        <source src="<?php echo $base_url; ?>/video/1070194624-hd.mov" type="video/mov">
+        Your browser does not support the video tag.
+    </video>
+</div>
 <!-- Carousel
     ================================================== -->
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
+<!--<div id="myCarousel" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner" role="listbox">
         <div class="item active">
             <img class="first-slide" src="<?php echo $base_url; ?>/images/command40years-1 (1).png" alt="First slide">
         </div>
-        <!--  <div class="item">
-            <img class="second-slide" src="<?php echo $base_url; ?>/images/CC-banner-1.jpg" alt="second slide">
-        </div> -->
-        <!--     <div class="item">
-            <img class="third-slide" src="<?php echo $base_url; ?>/images/CC-banner-2.jpg" alt="third slide">
-        </div>
-
-        <div class="item">
-            <img class="fourth-slide" src="<?php echo $base_url; ?>/images/CC-banner-3.jpg" alt="fourth slide">
-        </div>
-
-        <div class="item">
-            <img class="fifth-slide" src="<?php echo $base_url; ?>/images/CC-banner-4.jpg" alt="fifth slide">
-        </div> -->
-
-        <!-- Left and right controls -->
         <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
             <span class="icon-prev" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
@@ -32,8 +22,8 @@
             <span class="sr-only">Next</span>
         </a>
     </div>
-
-</div><!-- /.carousel -->
+</div> -->
+<!-- /.carousel -->
 
 <!-- 5 services
     ================================================== -->

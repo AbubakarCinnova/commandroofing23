a72daaee494fae585e138a190d55ebf2518abdf7
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-30 12:38:33 +0500

    Update about.php

--- about.php
+++ about.php
@@ -7,43 +7,22 @@
     <h1>About Us</h1>
     <div class="float-img"><img src="<?php echo $base_url; ?>/images/Dan Philpot Command Roofing.jpg"
             style="width:280px" alt="Owners Jessica and Dan Philpot Command Roofing ">
+        <p><strong>Dan Philpot Command Roofing.jpg</strong></p>
     </div>
     <br>
 
-    <p>Welcome to Command Roofing, a family-owned and operated business dedicated to delivering exceptional industrial
-        commercial roofing services. We have consistently been a front-runner in providing excellence and innovation
-        directly for our clients.</p>
+    <p> Welcome to Command Roofing, a family-owned business providing superior commercial and industrial roofing
+        services. With 40 years of trusted service in Southwest Ohio, our expert team ensures quality from installation
+        to maintenance in a safe work environment. Our commitment to safety, workmanship, and value makes us the
+        preferred choice for your commercial roofing needs.
 
-    <p>For over 40 years, Command Roofing has proudly remained a trusted name in the southwest Ohio area for industrial
-        commercial roofing services. Our enduring presence in the industry is a testament to our unwavering commitment
-        to
-        meeting clients' unique roofing needs with efficiency and service.</p>
-
-    <p>At the core of our success is our exceptional team of highly trained roofing professionals who bring our services
-        directly to you. Each member of our team is meticulously selected and rigorously trained to uphold our stringent
-        standards of excellence. This guarantees that every project we undertake is executed with the utmost
-        professionalism and a steadfast dedication to quality. Our collective knowledge and hands-on experience empower
-        us to tackle even the most intricate roofing challenges with confidence.</p>
-
-    <p>Command Roofing takes immense pride in its ability to provide comprehensive industrial commercial roofing
-        services
-        tailored to our client's specific requirements. From installation and maintenance to repairs and inspections,
-        our full spectrum of services is designed to optimize your roofing system's longevity and enhance its
-        performance. Recognizing the pivotal role a well-maintained roof plays in the success of your business, we are
-        committed to ensuring that your roofing needs are met reliably and efficiently.</p>
-
-    <p>As a family-owned and operated business, Command Roofing is guided by a commitment to excellence and integrity.
-        We continuously embrace advancements in roofing technology and techniques, positioning ourselves at the
-        forefront
-        of the industry. Our dedication to innovation, combined with our enduring family values, positions us to
-        persist as the preferred choice for industrial commercial roofing solutions in southwest Ohio for many years to
-        come.</p>
+    </p>
 
     <p><strong> Command Roofing is your trusted roofing partner, offering dependable and durable commercial and
-            industrial
-            roofing solutions that go beyond your expectations. Our mission is to provide top-notch services that you
-            can
-            rely on for all your industrial commercial roofing needs.</strong></p>
+            industrial roofing solutions that go beyond your expectations. Our mission is to provide top-notch services
+            in a safe work environment that you can rely on for all your commercial and industrial roofing needs.
+
+        </strong></p>
 
     <br>
     <p><a href="<?php echo $base_url; ?>/contact.php" class="btn btn-default">Contact Us </a> </p>

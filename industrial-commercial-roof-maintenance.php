6b9edf7dd945d9c7aefbb6c3f64dd17083696ad4
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-30 15:16:45 +0500

    updated roof maintenance

--- industrial-commercial-roof-maintenance.php
+++ industrial-commercial-roof-maintenance.php
@@ -1,31 +1,41 @@
 <?php include('header.php'); ?>
-<title>Industrial Commercial Roofing Maintenance | Command Roofing</title>
+<title>Commercial and Industrial Roofing Maintenance | Command Roofing</title>
+
+
 <meta name="description"
-    content=" At Command Roofing our commercial roofers perform maintenance tasks according to the maintenance plan to ensure the longevity and functionality of commercial and industrial roofs." />
+    content=" At Command Roofing our commercial roofers perform service tasks according to the maintenance plan to ensure the longevity and functionality of commercial and industrial roofs." />
 
 <?php include("menu.php"); ?>
 
 <div class="clearfix border-black"></div>
 <div class="container margin-bottom-lg margin-top-lg">
-    <div class="img-wrap"><img src="<?php echo $base_url; ?>/images/command roofing inspection.jpeg" style="width:65%"
-            alt="cri">
+    <div class="img-wrap"><img src="<?php echo $base_url; ?>/images/Commercial roof repair dayton -min.jpg"
+            style="width:65%" alt="cri">
     </div>
     <br>
 
-    <h1>Command Roofing's Industrial Commercial Roof Maintenance Program</h1>
-    <p>Command Roofing's Industrial Commercial Roof Maintenance Programs play a pivotal role in safeguarding your
+
+    <h1>Command Roofing's Commercial and Industrial Roof Maintenance Program</h1>
+
+
+
+    <p>Command Roofing's Commercial and Industrial Roof Maintenance Programs play a pivotal role in safeguarding your
         investment. By meticulously identifying both overt and concealed hazards that pose a threat to your building's
         roofing system, we empower you to significantly extend the operational life of your commercial roof. The
-        enduring performance of a commercial roof is predicated upon two crucial pillars: the quality of its initial
-        installation and the efficacy of its ongoing maintenance.</p>
-    <p>At Command Roofing, our commercial roofers perform maintenance tasks according to the maintenance plan to ensure
-        the longevity and functionality of commercial and industrial roofs.</p>
+        longevity
+        of a commercial roof is predicated upon two crucial pillars: the quality of its initial installation and the
+        efficacy of its ongoing maintenance.</p>
 
-    <h2>Industrial Commercial Roof Maintenance Program:</h2>
+
+
+    <p>At Command Roofing our commercial roofers perform service tasks according to the maintenance plan to ensure the
+        longevity and functionality of commercial and industrial roofs.</p>
+
+    <h2>Commercial and Industrial Roof Maintenance Program:</h2>
     <ul>
-        <li><strong>Regular Inspections:</strong> Our Industrial Commercial roofers conduct routine on-site inspections
-            with aerial drones and roof Infrared scans to identify any signs of damage, wear, or potential issues.
-            Inspections help catch problems early and prevent more extensive damage.</li>
+        <li><strong>Regular Inspections:</strong> Our commercial roofers conduct routine on-site inspections to identify
+            any signs of damage, wear, or potential issues. Inspections help catch problems early and prevent more
+            extensive damage.</li>
         <li><strong>Clearing Debris:</strong> Leaves, branches, dirt, and other debris can accumulate on the roof,
             clogging roof drains and causing water pooling. Our roofers will clear debris to prevent water damage and
             ensure proper drainage.</li>
@@ -35,12 +45,12 @@
             as vents, HVAC units, and pipes) can degrade. Our commercial roofers will inspect and reseal these areas to
             prevent leaks.</li>
         <li><strong>Repairing Flashing:</strong> Flashing is used to seal gaps and joints in the roof, such as around
-            vents and walls. Our commercial roofers will repair or replace damaged flashing to prevent water
+            vents, and walls. Our commercial roofers will repair or replace damaged flashing to prevent water
             infiltration.</li>
         <li><strong>Patching and Repairing Leaks:</strong> If a leak is identified, our commercial roofers will locate
             the source of the leak and patch or repair the damaged area to prevent further water damage.</li>
         <li><strong>Moss and Algae Removal:</strong> In damp environments, moss and algae can grow on the roof's
-            surface, which can lead to deterioration. Our Commercial roofers will remove these growths to prevent
+            surface, which can lead to deterioration. Our commercial roofers will remove these growths to prevent
             damage.</li>
         <li><strong>Checking Drains and Scuppers:</strong> Proper drainage is crucial to prevent water pooling. Our
             commercial roofers will clean and ensure the functionality of drains and scuppers.</li>
@@ -55,12 +65,11 @@
         problems that could lead to costly repairs or premature roof replacement. It also helps ensure the safety and
         functionality of the building's interior.</p>
 
-
-
     <h2>By investing in Command Roofing's robust maintenance program, you stand to gain:</h2>
     <ul>
         <li><strong>Extended Lifespan:</strong> Addressing potential issues proactively helps prolong the life of your
-            commercial roof, sparing you the substantial costs associated with premature replacement.</li>
+            commercial and industrial roof, sparing you the substantial costs associated with premature replacement.
+        </li>
         <li><strong>Cost Savings:</strong> Detecting and resolving problems early on prevents minor concerns from
             snowballing into major, costly repairs.</li>
         <li><strong>Business Continuity:</strong> Minimized disruptions due to unexpected roof issues allow your
@@ -78,15 +87,23 @@
         <li><strong>Peace of Mind:</strong> Knowing your roof is well-maintained provides peace of mind, allowing you to
             focus on your core business activities.</li>
     </ul>
-    <br>
-    <p>By emphasizing a comprehensive approach to commercial roof maintenance, you proactively fortify your investment
-        against the challenges of time and weather, while simultaneously reaping the rewards of a resilient and
+
+
+
+
+    <p>By emphasizing a comprehensive approach to commercial and industrial roof maintenance, you proactively fortify
+        your
+        investment against the challenges of time and weather, while simultaneously reaping the rewards of a resilient
+        and
         efficient roofing system.</p>
+
     <p><strong>Command Roofing is your trusted roofing partner, offering dependable and durable commercial and
-            industrial
-            roofing solutions that go beyond your expectations. Our mission is to provide top-notch services that you
-            can
-            rely on for all your roofing needs.</strong></p>
+            industrial roofing
+            solutions that go beyond your expectations. Our mission is to provide top-notch services in a safe work
+            environment
+            that you can rely on for all your roofing needs.</strong></p>
+
+
 
 
     <br>

da94bbbef46280ce84c6b700909684b721a787bd
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-30 15:22:16 +0500

    Update tpo-roofing.php

--- tpo-roofing.php
+++ tpo-roofing.php
@@ -1,7 +1,7 @@
 <?php include('header.php'); ?>
-<title>Industrial Commercial TPO Roofing System | Command Roofing</title>
+<title>TPO Commercial Industrial Roofing | Command Roofing</title>
 <meta name="description"
-    content=" TPO (Thermoplastic Polyolefin) commercial roofing is a single-ply roofing material that has gained popularity for its numerous advantages." />
+    content=" Command Roofing is your reliable roofing partner. We are experts in installing TPO roofing for industrial and commercial clients situated in southwest Ohio." />
 <?php include("menu.php"); ?>
 <div class="clearfix border-black"></div>
 <div class="container margin-bottom-lg margin-top-lg">
@@ -9,9 +9,16 @@
             alt="tpo">
     </div>
     <br>
-    <h1>TPO Commercial Roofing System</h1>
-    <p>TPO (Thermoplastic Polyolefin) commercial roofing is a single-ply roofing material that has gained popularity for
-        its numerous advantages. Here are the key advantages of using TPO roofing:</p>
+    <h1>TPO Roofing System</h1>
+
+    <p><strong>Command Roofing</strong> is your reliable roofing partner. We are experts in installing TPO roofing for
+        industrial and commercial clients situated in southwest Ohio. Our mission is to provide top-notch services in a
+        safe work environment that you can rely on for all your roofing needs.</p>
+
+    <h2>TPO (Thermoplastic Polyolefin) Roofing</h2>
+    <p>TPO (Thermoplastic Polyolefin) commercial and industrial roofing is a single-ply roofing material that has gained
+        popularity for its numerous advantages. Here are the key advantages of using TPO roofing:</p>
+
     <ul>
         <li><strong>Energy Efficiency:</strong> TPO roofing is available in light-colored membranes, which reflect
             sunlight and reduce heat absorption. This can lead to lower cooling costs during hot weather, making it an
@@ -28,27 +35,26 @@
         <li><strong>Lightweight:</strong> TPO roofing is lightweight compared to some other roofing materials, which can
             be beneficial for new construction and reroofing projects, as it reduces the load on the building's
             structure.</li>
-        <li><strong>Environmentally Friendly:</strong> TPO roofing is considered an environmentally friendly option as
-            it can be recycled at the end of its service life, reducing waste and promoting sustainability.</li>
         <li><strong>Chemical Resistance:</strong> TPO roofing is resistant to chemical exposure, making it suitable for
-            industrial and commercial buildings where chemical pollutants may be present.</li>
+            commercial and industrial buildings where chemical pollutants may be present.</li>
         <li><strong>Cost-Effective:</strong> TPO roofing is often more cost-effective compared to other roofing
             materials like PVC or EPDM, making it an attractive choice for budget-conscious projects.</li>
         <li><strong>Compatibility with Insulation:</strong> TPO roofing membranes can be easily adhered to or
             mechanically fastened over various types of roof insulation, providing effective insulation and improving
             the building's energy efficiency.</li>
     </ul>
+
     <p><strong>New Roofs:</strong> TPO is often chosen due to its energy efficiency, durability, and ease of
         installation. It offers an excellent solution for architects and contractors looking for a single-ply roofing
         system that can provide reliable performance and energy savings.</p>
+
     <p><strong>Reroofing:</strong> TPO roofing can be an efficient choice. It can be installed directly over the
         existing roof (in a roof-over or recovery installation) if the underlying structure is still in good condition.
         This approach reduces the cost and time associated with tearing off the old roof, making it a cost-effective
         option for refurbishing older buildings.</p>
-    <p><strong><em>Command Roofing</em> is your trusted roofing partner, offering dependable and durable commercial and
-            industrial roofing solutions that go beyond your expectations. Our mission is to provide top-notch services
-            that
-            you can rely on for all your roofing needs.</strong></p>
+
+    <p><strong>Quality Products, Superior Workmanship, and Service After The Sale Makes Our Roofing Installations
+            Extraordinary!</strong></p>
     <br>
     <p><a href="<?php echo $base_url; ?>/contact.php" class="btn btn-default">Contact Us </a> </p>
     <br>

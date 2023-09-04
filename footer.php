ba8d6d7d3a12d9cfa18ad95229676b027ae7c8dc
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-09-04 12:59:41 +0500

    updated footer

--- footer.php
+++ footer.php
@@ -1,18 +1,15 @@
 <footer>
     <div class="container">
         <div class="row pt-lg pb-lg">
-            <div class="col-md-2 ">
+            <div class="col-md-2 col-sm-12">
                 <div class="footer-logo mb-md">
                     <img src="<?php echo $base_url; ?>/images/logo.png" class="img-responsive" alt="">
                 </div>
                 <!--<p>Elastizell System, Inc is a concrete application specialist in Southwest Ohio. Providing unmatched expertise in geotechnical applications, Elastizell System, Inc is highly sought after  to present customized solutions to exceed your expectations</p>-->
             </div>
-            <div class="col-md-7">
+            <div class="col-md-7 col-sm-12">
                 <div class=" footer-item">
-                    <div class="col-12">
-                        <h4 class="service-area">Service Area</h4>
-                    </div>
-
+                    <h4 class="service-area">Service Area</h4>
                     <ul class="list-unstyled">
                         <div class="col-md-4 col-sm-12 col-xs-12 ">
                             <li><a href="<?php echo $base_url; ?>/service-area/dayton.php">Dayton, OH</a> </li>
@@ -40,7 +37,7 @@
                     </ul>
                 </div>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-2 col-sm-12">
                 <div class="contact-div">
                     <h4>Contact Us</h4>
                     <p> 2485 Arbor Blvd. <br>

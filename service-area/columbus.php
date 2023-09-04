da0ac7fa2e3cc50aa7d626a626e3ed55d11acd0a
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-09-04 10:35:20 +0500

    updated columbus

---
+++ service-area/columbus.php
@@ -0,0 +1,83 @@
+<?php include('../header.php'); ?>
+<title>Command Roofing - Commercial and Industrial Roofing | Columbus Ohio</title>
+<meta name="description"
+    content=" Command Roofing has solidified its position as the preferred option for exceptional industrial and commercial roofing services in Columbus, Ohio." />
+
+
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+
+<?php include("../menu.php"); ?>
+<div class="clearfix border-black"></div>
+<div class="container margin-bottom-lg margin-top-lg">
+
+    <h1>Columbus, Ohio</h1>
+
+    <h2>Columbus's Experts in Commercial and Industrial Roofing</h2>
+
+    <p>With a distinguished history of four decades, Command Roofing has solidified its position as the preferred option
+        for exceptional industrial and commercial roofing services in Columbus, Ohio. Rooted in being a family-owned and
+        operated business, we take great pride in our consistent delivery of top-tier services, all within a safe
+        working environment. Rely on Command Roofing for all your commercial and industrial roofing requirements in
+        Columbus.</p>
+
+    <h2>Elevating Excellence in Columbus, Ohio: Delivering Comprehensive Solutions for Diverse Commercial Roofing Needs
+    </h2>
+
+    <p>Drawing upon our expertise, our Commercial Roofing Services encompass a wide range of offerings, including annual
+        maintenance plans, leak detection, repairs, installations, and responsive emergency services.</p>
+
+    <p>Our esteemed clientele in Columbus, Ohio, and beyond comprises a diverse array of establishments, including
+        commercial buildings, malls, distribution facilities, factories, schools, hospitals, and government buildings.
+        These entities rely on a dependable Commercial Roofing Company to oversee the monitoring, maintenance, and
+        repair of their Commercial and Industrial Properties. We offer tailored maintenance programs for existing roofs
+        and extend emergency services to all our clients. Furthermore, our collaboration extends to Property Owners
+        within the Columbus area. We work seamlessly alongside their Builders on Commercial Remodels and New
+        Construction projects.</p>
+
+    <h2>Our Commercial and Industrial Roofing Services in Columbus, Ohio:</h2>
+
+    <ul>
+        <li>Commercial Roof Leak Investigations and Repairs</li>
+        <li>New Commercial Roofs</li>
+        <li>New Industrial Roofs</li>
+        <li>Commercial Roof Infrared Scans</li>
+        <li>Commercial and Industrial Roofing Restorations</li>
+        <li>Roof modifications</li>
+        <li>Roof modifications for Solar Systems</li>
+        <li>Re-Roofing</li>
+        <li>Commercial Roofing Repairs</li>
+        <li>Industrial Roofing Repairs</li>
+        <li>Emergency Roofing Repairs</li>
+        <li>Scheduled Roofing Maintenance Programs</li>
+    </ul>
+
+    <h2>Command Roofing installs roof systems for all of the major single-ply, built-up, and modified bitumen roof
+        system
+        manufacturers. Our major product lines in Columbus, Ohio include:</h2>
+
+    <ul>
+        <li>EPDM Roofing Systems</li>
+        <li>PVC Roofing Systems</li>
+        <li>TPO Roofing Systems</li>
+        <li>Coating Systems (maintenance)</li>
+        <li>Sheet Metal (architectural) Roofing</li>
+        <li>Built-up (asphalt & coal tar pitch)</li>
+        <li>APP Roofing Systems</li>
+        <li>SBS Roofing Systems</li>
+        <li>Standing Seam Metal Roofing</li>
+    </ul>
+
+    <p><strong>Quality Products, Superior Workmanship, and Service After The Sale Make Our Columbus, Ohio Roofing
+            Installations
+            Extraordinary!</strong></p>
+
+    <br>
+    <p><a href="<?php echo $base_url; ?>/contact.php" class="btn btn-default">Contact Us </a> </p>
+    <br>
+
+</div>
+<div class="clearfix"></div>
+
+<?php include('../footer.php'); ?>

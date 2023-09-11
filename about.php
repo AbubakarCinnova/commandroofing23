7f2ef8e4e1f3774744725c9c87fe0fb16e372b94
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-09-11 11:22:46 +0500

    updated about us pic

--- about.php
+++ about.php
@@ -6,10 +6,11 @@
 
 
     <h1>About Us</h1>
-    <div class="float-img"><img src="<?php echo $base_url; ?>/images/Dan Philpot Command Roofing.jpg"
+    <div class="float-img"><img
+            src="<?php echo $base_url; ?>/images/Dan and Jessica Photo- command roofing-compressed (1).jpg"
             style="width:280px; margin-bottom:15px;" alt="Owners Jessica and Dan Philpot Command Roofing ">
 
-        <p class="text-center"><strong>Dan Philpot Command Roofing</strong></p>
+        <p class="text-center"><strong>Dan and Jessica Phlipot Owners Command Roofing</strong></p>
     </div>
     <br>
 

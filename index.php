01770c805a40c1c84f5348af3545152d1ab07199
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-09-13 10:59:12 +0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <source src="<?php echo $base_url; ?>/video/command-banner-video.mp4" type="video/mp4">
         Your browser does not support the video tag.
     </video> -->
-    <div style="padding:56.25% 0 0 0;position:relative;"><iframe
+    <div style="padding:56.25% 0 0 0;position:relative;"><iframe id="vimeo-video"
             src="https://player.vimeo.com/video/861793942?h=656d1f276a&autoplay=1&loop=1&title=0&byline=0&portrait=0"
             style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"
             allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div>
